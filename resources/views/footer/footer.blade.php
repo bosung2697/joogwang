eeb2e910fc96c351e1b7c2eae5613e0f4857742f
bosung2697 <36359088+bosung2697@users.noreply.github.com>
2018-07-06 10:13:11 +0900

    footer

--- resources/views/footer/footer.blade.php
+++ resources/views/footer/footer.blade.php
@@ -1,28 +1,28 @@
 <div class="footer">
     <div class="footer_child" style="border-bottom: 0.2px solid white;">
         <div class="grid-item" style="cursor:pointer;" onclick="location.href='{{url('/intro')}}'">About Us</div>
-        <div class="grid-item"  style="cursor:pointer;" onclick="location.href='{{url('/product_intro')}}'">Product</div>
+        <div class="grid-item" style="cursor:pointer;" onclick="location.href='{{url('/product_intro')}}'">Product</div>
         <div class="grid-item">Career</div>
         <div class="grid-item" onclick="location.href='{{url('/newsroom')}}'">PR</div>
         <div class="grid-item">CSR</div>
     </div>
     <div class="footer_child">
         <div class="grid-item" style="cursor:pointer;" onclick="location.href='{{url('/intro')}}'">ceo 인사말</div>
-        <div class="grid-item"  style="cursor:pointer;" onclick="location.href='{{url('/product_intro')}}'">제품소개</div>
+        <div class="grid-item" style="cursor:pointer;" onclick="location.href='{{url('/product_intro')}}'">제품소개</div>
         <div class="grid-item">인재상</div>
         <div class="grid-item" style="cursor:pointer;" onclick="location.href='{{url('/newsroom')}}'">뉴스룸</div>
         <div class="grid-item">나눔경영</div>
     </div>
     <div class="footer_child">
         <div class="grid-item" style="cursor:pointer;" onclick="location.href='{{url('/company_status')}}'">기업현황</div>
-        <div class="grid-item"  style="cursor:pointer;" onclick="location.href='{{url('/RandD')}}'">연구개발(연구소)</div>
+        <div class="grid-item" style="cursor:pointer;" onclick="location.href='{{url('/RandD')}}'">연구개발(연구소)</div>
         <div class="grid-item">인사제도</div>
         <div class="grid-item">Library</div>
         <div class="grid-item">품질경영</div>
     </div>
     <div class="footer_child">
-        <div class="grid-item"  style="cursor:pointer;" onclick="location.href='{{url('/tob')}}'">조직도</div>
-        <div class="grid-item"  style="cursor:pointer;" onclick="location.href='{{url('/productprocess')}}'">생산공정</div>
+        <div class="grid-item" style="cursor:pointer;" onclick="location.href='{{url('/tob')}}'">조직도</div>
+        <div class="grid-item" style="cursor:pointer;" onclick="location.href='{{url('/productprocess')}}'">생산공정</div>
         <div class="grid-item">복리후생</div>
         <div class="grid-item">FAQ</div>
         <div class="grid-item">윤리경영</div>
@@ -36,7 +36,7 @@
     </div>
     <div class="footer_child">
         <div class="grid-item" style="cursor:pointer;" onclick="location.href='{{url('/awards')}}'">수상실적</div>
-        <div class="grid-item"style="cursor:pointer;" onclick="location.href='{{url('/ventureawards')}}'">인증수상</div>
+        <div class="grid-item" style="cursor:pointer;" onclick="location.href='{{url('/ventureawards')}}'">인증수상</div>
         <div class="grid-item">채용절차</div>
         <div class="grid-item">공지사항</div>
         <div class="grid-item"></div>
@@ -54,5 +54,18 @@
         <div class="grid-item"></div>
         <div class="grid-item"></div>
         <div class="grid-item"></div>
+    </div>
+</div>
+<div style="display:flex; justify-content: space-between; align-items: center; color:#808080; font-size:0.5vw; font-weight:500;">
+        <div style="margin:auto; padding:0 0 0 11vw; letter-spacing:0.02vw;"><div style="width:100%; padding:0; text-align:left;">
+            <div>본사 : 경북 구미시 1공단로 10길 55(공단동) / TEl : 054) 461-3732 / FAX : 054)461-3735</div>
+            <div>2공장 : 경북 구미시 1공단로 7길 36-13(공단동) / TEL : 054) 465-3763 / FAX : 054) 465-3764</div>
+            <div>광주공장 : 광주 광역시 북구 첨단연신로 70(연제동) / TEL :062) 972-3731 / FAX : 062) 972-3730</div>
+            <div>주광정밀(주) 대표 : 윤재호 Copyright(c)2016 JUKWANG.CO.KR All rights reserved.</div>
+            </div></div>
+    <div style="text-align:right; display:flex; justify-content:space-between; align-items:center;  width:22%; padding: 1vw 1vw 0 1vw;">
+        <div>이용약관</div>
+        <div>개인정보처리방침</div>
+        <div>사이트맵</div>
     </div>
 </div>
